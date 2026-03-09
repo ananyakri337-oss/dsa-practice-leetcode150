6d23db68952bb394520ecb6ed452a46289d1f549
Ananya Kumari <ananyakri337@gmail.com>
2026-03-09 23:58:46 +0530

    Added Two Sum solution

---
+++ Arrays/two_sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numToIndex = {}
+        for i, num in enumerate(nums):
+            if target - num in numToIndex:
+                return numToIndex[target - num], i
+            numToIndex[num] = i
+
+# Problem 1: Two Sum
+# Link: https://leetcode.com/problems/two-sum/  
